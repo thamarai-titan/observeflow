e59716206c80e667a24fe0329862de603e16ddb1
Thamarai <tmanalan710@gmail.com>
2026-02-23 22:00:10 +0530

    creating post route

--- apps/api/src/modules/project/project.controller.ts
+++ apps/api/src/modules/project/project.controller.ts
@@ -1,8 +1,9 @@
 import type { Request, Response } from "express";
+import { ProjectSchema, type ProjectType } from "./project.schema";
 
-export const CreateProjectController = async () => {
+export const CreateProjectController = async (req: Request, res: Response) => {
     try {
-        
+        const data: ProjectType = ProjectSchema.parse(req.body)
     } catch (error) {
         
     }
