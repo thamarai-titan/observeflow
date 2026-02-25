e0dfbbc8e496389d42db0012eee2ac92cb73173d
Thamarai <tmanalan710@gmail.com>
2026-02-25 22:58:48 +0530

    create project service added

--- apps/api/src/modules/project/project.service.ts
+++ apps/api/src/modules/project/project.service.ts
@@ -1,9 +1,31 @@
+import { prisma } from "../../lib/prisma";
+import type { ProjectType } from "./project.schema";
+import { randomBytes } from "crypto";
 
-
-export const CreateProjectService = async ()=> {
+export const CreateProjectService = async (data: ProjectType, userId: string)=> {
     try {
-        
+        const{
+            name,
+            description
+        } = data
+
+        const apiKey = randomBytes(32).toString('hex')
+
+        const project = await prisma.projects.create({
+            data: {
+                name,
+                description,
+                apiKey: apiKey,
+                userId: userId
+            },
+            select: {
+                id: true,
+                name: true,
+                description: true
+            }
+        })
+        return project
     } catch (error) {
-        
+        throw error
     }
 }
