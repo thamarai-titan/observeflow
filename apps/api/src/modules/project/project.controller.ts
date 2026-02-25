0ff5f9bf26626b782bf2559beab18ac0a7d19ca3
Thamarai <tmanalan710@gmail.com>
2026-02-25 22:58:25 +0530

    create project controller added

--- apps/api/src/modules/project/project.controller.ts
+++ apps/api/src/modules/project/project.controller.ts
@@ -1,10 +1,35 @@
-import type { Request, Response } from "express";
+import { response, type Request, type Response } from "express";
 import { ProjectSchema, type ProjectType } from "./project.schema";
+import { CreateProjectService } from "./project.service";
+import { responses } from "../../lib/responses";
+import { Prisma } from "@prisma/client";
 
 export const CreateProjectController = async (req: Request, res: Response) => {
     try {
         const data: ProjectType = ProjectSchema.parse(req.body)
-    } catch (error) {
-        
+
+        const userId = req.userId
+
+        const project = await CreateProjectService(data, userId)
+
+        res.status(200).json(responses.success({
+            project
+        }))
+    } catch (error: any) {
+        if (error.name === "ZodError") {
+      return res.status(400).json(responses.error("INVALID_INPUT"));
     }
-} 
+
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2002") {
+        return res.status(409).json(responses.error("TOOL_ALREADY_EXISTS"));
+      }
+
+      if (error.code === "P2003") {
+        return res.status(400).json(responses.error("INVALID_CATEGORY"));
+      }
+    }
+
+    return res.status(500).json(responses.error("INTERNAL_SERVER_ERROR"));
+  }
+}
