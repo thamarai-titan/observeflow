d1352180cfb46f447297673f48ebd5e8d5347108
Thamarai <tmanalan710@gmail.com>
2026-02-25 22:48:47 +0530

    user profile route completed

--- apps/api/src/modules/user/user.route.ts
+++ apps/api/src/modules/user/user.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { verifyToken } from "../../middleware/auth.middleware";
+import { UserDetailsController } from "./user.controller";
 
 
 const router = Router()
 
-
+router.get("/auth/profile", verifyToken, UserDetailsController)
 
 export default router
