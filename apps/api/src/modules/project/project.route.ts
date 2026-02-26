4f75ccb7c08b422b0c342b28d45576865fa68f1d
Thamarai <tmanalan710@gmail.com>
2026-02-26 21:03:54 +0530

    route for apikey rotation added

--- apps/api/src/modules/project/project.route.ts
+++ apps/api/src/modules/project/project.route.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { verifyToken } from "../../middleware/auth.middleware";
-import { CreateProjectController, DeleteProjectController, GetallProjectsController } from "./project.controller";
+import { CreateProjectController, DeleteProjectController, GetallProjectsController, RotateApiKeyController } from "./project.controller";
 
 const router = Router()
 
 router.post("/projects", verifyToken, CreateProjectController)
 router.get("/projects", verifyToken, GetallProjectsController)
 router.delete("/projects/:id", verifyToken, DeleteProjectController)
-
+router.post("/projects/:id/regenerate-key", verifyToken, RotateApiKeyController)
 export default router
