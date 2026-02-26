076b3d7ca0a82445ff464368cb297b6aa6ba245b
Thamarai <tmanalan710@gmail.com>
2026-02-26 13:01:05 +0530

    projects routes added

--- apps/api/src/modules/project/project.route.ts
+++ apps/api/src/modules/project/project.route.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
+import { verifyToken } from "../../middleware/auth.middleware";
+import { CreateProjectController, DeleteProjectController, GetallProjectsController } from "./project.controller";
 
 const router = Router()
 
-
+router.post("/projects", verifyToken, CreateProjectController)
+router.get("/projects", verifyToken, GetallProjectsController)
+router.delete("/projects/:id", verifyToken, DeleteProjectController)
 
 export default router
