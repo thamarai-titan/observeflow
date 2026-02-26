4a855c6656580c4eef5ba825acebffcd5eb5ec4d
Thamarai <tmanalan710@gmail.com>
2026-02-26 12:58:00 +0530

    controller for projects completed

--- apps/api/src/modules/project/project.controller.ts
+++ apps/api/src/modules/project/project.controller.ts
@@ -1,8 +1,12 @@
-import { response, type Request, type Response } from "express";
+import type { Request, Response } from "express";
 import { ProjectSchema, type ProjectType } from "./project.schema";
-import { CreateProjectService } from "./project.service";
+import { CreateProjectService, DeleteProjectService, GetallProjectService } from "./project.service";
 import { responses } from "../../lib/responses";
 import { Prisma } from "@prisma/client";
+
+type Params = {
+  projectId: string
+}
 
 export const CreateProjectController = async (req: Request, res: Response) => {
     try {
@@ -31,5 +35,47 @@ export const CreateProjectController = async (req: Request, res: Response) => {
     }
 
     return res.status(500).json(responses.error("INTERNAL_SERVER_ERROR"));
+  }
+}
+
+
+export const GetallProjectsController = async (req: Request, res: Response) => {
+  try {
+    const userId = req.userId
+
+    const projects = await GetallProjectService(userId)
+
+    if(!projects.length){
+      return res.status(400).json(responses.error("NO_RPOJECTS"))
+    }
+
+    return res.status(200).json({
+      projects
+    }) 
+  } catch (error) {
+    return res.status(500).json(responses.error("INTERNEL_SERVER"))
+  }
+}
+
+export const DeleteProjectController = async (req: Request<Params>, res: Response) => {
+  try {
+    const userId = req.userId
+    const projectId = req.params.projectId as string
+
+    const project = await DeleteProjectService(userId, projectId)
+
+    if (project){
+      return res.status(200).json(responses.success({
+        "message": "Project deleted Successfully"
+      }))
+    }
+  } catch (error: any) {
+    if(error instanceof Prisma.PrismaClientKnownRequestError){
+      if(error.code === "P2025"){
+        return res.status(400).json(responses.error("NO_ITEM_TO_DELETE"))
+      }
+    }
+
+    return res.status(500).json(responses.error("INTERNEL_SERVER_ERROR"))
   }
 }
