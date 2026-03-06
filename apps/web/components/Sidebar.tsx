792acbd30d83790fe0266e1bfc9674a306c4000d
Thamarai <tmanalan710@gmail.com>
2026-03-06 21:22:58 +0530

    sidevar added

--- apps/web/components/Sidebar.tsx
+++ apps/web/components/Sidebar.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
-
-const Sidebar = () => {
+export default function Sidebar() {
   return (
-    <div>
-      
-    </div>
-  )
+    <aside className="w-60 border-r h-screen p-4">
+      <ul className="space-y-4">
+        <li>Dashboard</li>
+        <li>Services</li>
+        <li>Logs</li>
+        <li>Alerts</li>
+        <li>Settings</li>
+      </ul>
+    </aside>
+  );
 }
-
-export default Sidebar
